bb3ba17704bbd505db73caea5348bfcaec9d8688
sydnik <sydnik3@gmail.com>
2021-06-02 16:23:09 +0300

    Курс НБРБ
    Реализовал класс который узнает курс НБРБ и записывает его в файл.

--- src/main/java/org/sydnik/createContract/data/Currency.java
+++ src/main/java/org/sydnik/createContract/data/Currency.java
@@ -1,68 +1,80 @@
 package org.sydnik.createContract.data;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.net.HttpURLConnection;
 import java.net.URL;
-import java.net.http.HttpResponse;
+import java.text.SimpleDateFormat;
 import java.util.Date;
-import java.util.Objects;
 
-public class Currency {
-    private String name = "Euro" ;
-    private int code = 292;
+public class Currency implements Serializable {
     private double value;
-    Date date;
-
-    public Currency() {
-
+    private String date;
+    public static Currency createCurrency(){
+        Currency currency = Currency.load();
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd MM YYYY");
+        if(currency ==null){
+            currency = new Currency();
+            currency.save();
+        } else if(!currency.getDate().equals(simpleDateFormat.format(new Date()))){
+            currency = new Currency();
+            currency.save();
+        }
+        return currency;
     }
-
-    public void setValue(double value) {
-        this.value = value;
+    private Currency() {
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd MM YYYY");
+        date = simpleDateFormat.format(new Date());
+        checkKurs();
     }
-
     public double getValue() {
         return value;
     }
-
-    public String getName() {
-        return name;
+    public String getDate() {
+        return date;
     }
 
-    public int getCode() {
-        return code;
-    }
-
-    public void ChechKurs() {
-            String nameSite = "http://www.nbrb.by/API/ExRates/Rates/" + code;
+    private void checkKurs() {
+            String nameSite = "https://www.nbrb.by/API/ExRates/Rates/292";//292 - курс евро.
             HttpURLConnection connection = null;
             String lineString = "";
             try {
                 connection = (HttpURLConnection) new URL(nameSite).openConnection();
                 connection.setRequestMethod("GET");
                 connection.setUseCaches(false);
-                connection.setConnectTimeout(3000);
+                connection.setConnectTimeout(1000);
                 connection.setReadTimeout(500);
-
                 connection.connect();
                 if (HttpURLConnection.HTTP_OK == connection.getResponseCode()) {
                     BufferedReader in = new BufferedReader(new InputStreamReader((connection.getInputStream())));
-
-
                     lineString = in.readLine();
                     in.close();
                 }
-                System.out.println(lineString);
             } catch (Exception e) {
             }
-            System.out.println(lineString);
-            int i = lineString.lastIndexOf("OfficialRate\":");
-            lineString.substring(i + 14, i + 20);
-            value = (Double.parseDouble(lineString));
-            System.out.println(value);
-
+            String[] newLine = lineString.split("OfficialRate\":");
+            newLine[1] = newLine[1].replace("}","");
+            value = (Double.parseDouble(newLine[1]));
     }
-    public void check () {
+    public void save(){
+        try {
+            FileOutputStream fileOutput = new FileOutputStream("settingManager/currency.dat");
+            ObjectOutputStream outputStream = new ObjectOutputStream(fileOutput);
+            outputStream.writeObject(this);
+            fileOutput.close();
+            outputStream.close();
+        } catch (Exception e) {
+        }
+    }
+    public static Currency load(){
+        try {
+            FileInputStream fiStream = new FileInputStream("settingManager/currency.dat");
+            ObjectInputStream objectStream = new ObjectInputStream(fiStream);
+            Object object = objectStream.readObject();
+            fiStream.close();
+            objectStream.close();
+            return (Currency) object;
+        }  catch (Exception e) {
+        }
+        return null;
     }
 }
